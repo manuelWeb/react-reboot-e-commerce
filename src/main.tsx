5e040d37972c600238787b33780d8806268a3360
manuelWeb <devries.webdesigner@gmail.com>
2026-07-26 22:34:11 +0200

    feat(mocks): enable API mocking in development

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+  const shouldEnableMocking =
+    import.meta.env.DEV || import.meta.env.VITE_ENABLE_MOCKS === 'true'
+  if (!shouldEnableMocking) {
+    return
+  }
+  const { worker } = await import('./mocks/browser')
+
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
