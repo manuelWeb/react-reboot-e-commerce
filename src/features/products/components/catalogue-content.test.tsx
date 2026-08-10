91acac7a115550edbc8c5eb478e0ac1de833ceb6
manuelWeb <devries.webdesigner@gmail.com>
2026-08-10 12:45:53 +0200

    test: add shared application providers

--- src/features/products/components/catalogue-content.test.tsx
+++ src/features/products/components/catalogue-content.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@/test/test-utils'
 import { userEvent } from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
 import { CatalogueContent } from './catalogue-content'
